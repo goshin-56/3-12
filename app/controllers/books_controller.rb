57119f7b240008add590bbfa70139a61ed346ab7
goshin-56 <102846714+goshin->
2022-04-06 13:21:00 +0000

    [Add]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,26 @@
 class BooksController < ApplicationController
   def index
+    
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path
+    else
+      render :index
+    end
   end
 
   def show
+    @book = Book.find(params[id])
   end
 
   def edit
+  end
+  
+  private
+  def book_params
+    params.repuire(:book).permit(:title,:body)
   end
 end
