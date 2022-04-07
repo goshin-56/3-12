9518d224677e3240c7bec25dcbc4f2995ce6421a
goshin-56 <102846714+goshin->
2022-04-07 08:07:38 +0000

    [Finish]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,7 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
-    @book = Book.new(book_params)
+    @book = Book.new
   end
 
   def create
@@ -10,27 +10,37 @@ class BooksController < ApplicationController
       flash[:notice] = "successfully"
       redirect_to book_path(@book.id)
     else
+      @books = Book.all
       render :index
     end
   end
 
   def show
-    @book = Book.find(params[id])
+    @book = Book.find(params[:id])
   end
 
   def edit
-    @book = Book.find(params[id])
+    @book = Book.find(params[:id])
   end
-  
+
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "successfully"
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
     book = Book.find(params[:id])
-    book.update(list_params)
-    flash[:notice] = "successfully"
-    redirect_to book_path(book.id)
+    book.destroy
+    redirect_to books_path
   end
 
   private
   def book_params
-    params.permit(:title,:body)
+    params.require(:book).permit(:title,:body)
   end
 end
